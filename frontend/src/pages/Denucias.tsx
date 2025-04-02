b8c52895f2d3c73e67af7a3f25dda4408044ca93
Medeiros096 <wellingtonmedeiros096@gmail.com>
2025-04-02 17:05:35 -0300

    fazendo front da pagina denuncia

--- frontend/src/pages/Denucias.tsx
+++ frontend/src/pages/Denucias.tsx
@@ -9,22 +9,32 @@ function Denuncias() {
         <div>
           <h1>Página de Denuncias</h1>
         </div>
-        <div className="formulario-denuncia">
-        <label htmlFor="foto"></label>
-        <input
-                    type="file"
-                    id="foto"
-                    name="foto"
-                    accept="image/*"
-                    // onChange={handleFileChange}
-                    required
-                />
-                <br /><br />
-                <label>Descriçao
-                  <br />
-                <textarea name="sdwvwv" id="" cols="30" rows="8"></textarea></label>
-                <br />
-                <button type="submit">Enviar</button>
+        {/* <div className="formulario-denuncia"> */}
+        <div>
+          <input className="escolha-arquivo"
+            type="file"
+            id="foto"
+            name="foto"
+            accept="image/*"
+            // onChange={handleFileChange}
+            required
+          />
+          <br /><br />
+          <label className="descricao">Descriçao
+            <br />
+            <textarea name="" id="" cols="30" rows="8"></textarea>
+          </label>
+          
+          <div className="denuncia-end">
+            Endereço: <br />
+            <input type="text" placeholder="Digite um cep valido" />
+            <br />
+            <input type="text" placeholder="Endereço" />
+            <br />
+            <input type="text" placeholder="Numero" />
+            <br />
+            <button type="submit">Enviar</button>
+          </div>
         </div>
       </div>
       <Footer />
