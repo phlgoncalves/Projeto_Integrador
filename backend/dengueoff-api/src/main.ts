6889ca660b8e656cf099ce1e10ac052ac4ffea51
Pedro Goncalves <peedro.goncalves@gmail.com>
2025-04-05 16:07:22 -0300

    Documentação do Swagger realizada no main

--- backend/dengueoff-api/src/main.ts
+++ backend/dengueoff-api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,17 +15,17 @@ async function bootstrap() {
     }),
   );
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('API Dengue-Off')
-  //   .setDescription(
-  //     'A Api é para um projeto social de denuncias contra casos de dengue que batem recordes a cada ano',
-  //   )
-  //   .setVersion('1.0')
-  //   .addTag('usuario')
-  //   .addTag('denuncia')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('API Dengue-Off')
+    .setDescription(
+      'A Api é para um projeto social de denuncias contra casos de dengue que batem recordes a cada ano',
+    )
+    .setVersion('1.0')
+    .addTag('usuarios')
+    .addTag('denuncias')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
