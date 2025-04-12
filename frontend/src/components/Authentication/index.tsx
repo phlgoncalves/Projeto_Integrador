323e1e3edef6674433507b6b1fd1e144fe71f425
PEDRO HENRIQUE LOPES GONCALVES <pedro.hlgoncalves@senacsp.edu.br>
2025-04-11 22:11:55 -0300

    funções do login

--- frontend/src/components/Authentication/index.tsx
+++ frontend/src/components/Authentication/index.tsx
@@ -1,25 +1,37 @@
+import { ChangeEvent, useState } from "react"
 import { Link } from "react-router-dom"
 
 function Authentication() {
+    const [email, setEmail] = useState('');
+    const handleAddEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+    }
+    const [senha, setSenha] = useState('');
+    const handleAddSenha = (e: ChangeEvent<HTMLInputElement>) => {
+        setSenha(e.target.value)
+    }
+
     return (
         <>
             <div>
                 <form className="authentication" >
                     <label>Email</label>
-                    <input 
-                        type="email" 
-                        name="email" 
+                    <input
+                        type="email"
+                        name="email"
                         id="email"
+                        onChange={handleAddEmail}
                         required
                     />
                     <label>Senha</label>
-                    <input 
-                        type="password" 
-                        name="senha" 
+                    <input
+                        type="password"
+                        name="senha"
                         id="senha"
+                        onChange={handleAddSenha}
                         required
                     />
-                    <button type="submit" className="button-login">
+                    <button type="submit" className="button-login" >
                         Entrar
                     </button>
                     <Link to={'/registro'} className="botao-registro">Não tem conta ainda? <br />Registre-se aqui</Link>
